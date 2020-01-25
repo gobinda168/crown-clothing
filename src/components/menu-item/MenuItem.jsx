af9abc95e30ce23b92faee0bad01cb14bbd6bc5d
Gobinda Das <gobinda168@gmail.com>
2020-01-25 13:35:43 +0530

    Homepage component separated and style applied completed all styling

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,39 +1,17 @@
 import React from "react";
 import "./menu-item.style.scss";
-const MenuItem = () => {
+const MenuItem = ({ section: { title, imageUrl } }) => {
   return (
-    <>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span>SHOP NOW</span>
-        </div>
+    <div className="menu-item">
+      <div
+        className="backgroundImage"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span>SHOP NOW</span>
       </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKET</h1>
-          <span>SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNEAKERS</h1>
-          <span>SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMEN</h1>
-          <span>SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MEN</h1>
-          <span>SHOP NOW</span>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
