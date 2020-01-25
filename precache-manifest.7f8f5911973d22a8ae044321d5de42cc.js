ced17940d6faec1a70573e6420c5886c98624f22
Gobinda Das <gobinda168@gmail.com>
2020-01-25 17:30:33 +0530

    Updates

---
+++ precache-manifest.7f8f5911973d22a8ae044321d5de42cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c5e184a28d0f292afe1e4de7a9c626",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "d617393bedd6cf11ea53",
+    "url": "/crown-clothing/static/css/main.ffe7bd46.chunk.css"
+  },
+  {
+    "revision": "4ebc65717b59b274fcd7",
+    "url": "/crown-clothing/static/js/2.079973a0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/crown-clothing/static/js/2.079973a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d617393bedd6cf11ea53",
+    "url": "/crown-clothing/static/js/main.c8f4b63b.chunk.js"
+  },
+  {
+    "revision": "01ffdf8a6afe7ae6bb37",
+    "url": "/crown-clothing/static/js/runtime-main.8dbb2be4.js"
+  }
+]);
