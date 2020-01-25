a62723dfdd27097bba993a41b53fd5910e3f9794
Gobinda Das <gobinda168@gmail.com>
2020-01-26 02:30:27 +0530

    Added header component and linked  to shop and contact pages also added the svg logo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { Homepage } from "./pages/homepage/Homepage";
 import { Route, Switch } from "react-router-dom";
 import Shop from "./pages/shop/Shop";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={Shop} />
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.style.scss";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+          <Link to='/shop' className="option">SHOP</Link>
+          <Link to='/shop' className="option">CONTACT</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
